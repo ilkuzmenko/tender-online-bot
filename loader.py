1812c4b66d3aabbb2cf77acdf71e6ff8512328bf
ilkuzmenko <illia.kuzmenko@icloud.com>
2021-08-11 14:04:10 +0300

    Add db dependencies

--- loader.py
+++ loader.py
@@ -1,10 +1,21 @@
+import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types
-import config
 
-bot = Bot(token=config.TOKEN, parse_mode=types.ParseMode.HTML)
-dp = Dispatcher(bot)
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+from config import TOKEN
+from utils.db.sql import create_pool, create_db
+
 
 logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
-                    level=logging.INFO,
-                    )
+                    level=logging.INFO)
+loop = asyncio.get_event_loop()
+
+
+storage = MemoryStorage()
+
+bot = Bot(token=TOKEN, parse_mode="HTML")
+dp = Dispatcher(bot, storage=storage)
+
+db = loop.run_until_complete(create_pool())
