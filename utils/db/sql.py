e9f28945269616bb60d62851c336fe73f7c08564
ilkuzmenko <illia.kuzmenko@icloud.com>
2021-08-11 14:03:13 +0300

    Add file slq.py

---
+++ utils/db/sql.py
@@ -0,0 +1,34 @@
+import asyncio
+import os
+import sys
+
+import asyncpg
+import logging
+
+from config import HOST, PG_PASS, PG_USER
+
+logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
+                    level=logging.INFO)
+
+
+async def create_db():
+    create_db_command = open(os.path.join(sys.path[0], "utils/db/create_db.sql"), "r").read()
+
+    logging.info("Connecting to database...")
+    conn: asyncpg.Connection = await asyncpg.connect(user=PG_USER, password=PG_PASS, host=HOST)
+
+    try:
+        await conn.execute(create_db_command)
+    except asyncpg.exceptions.DuplicateTableError:
+        pass
+    await conn.close()
+    logging.info("Table USERS created")
+
+
+async def create_pool():
+    return await asyncpg.create_pool(user=PG_USER, password=PG_PASS, host=HOST)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(create_db())
