fd68f790486e86e6e360b093a9f9537601ea5580
ilkuzmenko <illia.kuzmenko@icloud.com>
2021-08-11 14:01:43 +0300

    Add sql table create file

---
+++ utils/db/create_db.sql
@@ -0,0 +1,18 @@
+create table if not exists users
+(
+	id serial
+		constraint users_pk
+			primary key,
+	user_id bigint,
+	phone bigint,
+	first_name text,
+	last_name text,
+	blog boolean
+);
+
+alter table users
+    owner to postgres;
+
+create unique index if not exists users_user_id_uindex
+	on users (user_id);
+
