732f9d747d0d37af131ec7917106e4125719460b
ilkuzmenko <illia.kuzmenko@icloud.com>
2021-08-13 00:43:31 +0300

    Add subscribe_user to blog

--- utils/db/comands.py
+++ utils/db/comands.py
@@ -9,3 +9,8 @@ async def add_user(user_id, phone, first_n, last_n):
     ON CONFLICT DO NOTHING
     ''', user_id, phone, first_n, last_n)
     logging.info("User " + str(user_id) + " added to table")
+
+
+async def subscribe_user(status, user_id):
+    await db.execute('UPDATE users SET blog = $1 WHERE user_id = $2', status, user_id)
+    logging.info("User " + str(user_id) + " subscribe blog status " + str(status))
