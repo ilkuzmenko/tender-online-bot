695d2f80b6d5cc9ddc194a82548574dba4d54b17
ilkuzmenko <illia.kuzmenko@icloud.com>
2021-08-17 22:38:44 +0300

    Add blog to db function and blog out for user

--- utils/db/comands.py
+++ utils/db/comands.py
@@ -1,5 +1,5 @@
+import datetime
 import logging
-
 from loader import db
 
 
@@ -11,6 +11,26 @@ async def add_user(user_id, phone, first_n, last_n):
     logging.info("User " + str(user_id) + " added to table")
 
 
+async def add_blog(title, link, date_post):
+    await db.execute('''
+    INSERT INTO blog (title, link, date_post) VALUES ($1, $2, $3)
+    ON CONFLICT DO NOTHING
+    ''', title, link, datetime.datetime.strptime(date_post, '%d.%m.%Y'))
+    logging.info("Blog " + str(title) + " added to table")
+
+
 async def subscribe_user(status, user_id):
     await db.execute('UPDATE users SET blog = $1 WHERE user_id = $2', status, user_id)
     logging.info("User " + str(user_id) + " subscribe blog status " + str(status))
+
+
+async def get_blog_page():
+    blog_table = await db.fetch('''SELECT id, title, link, date_post FROM blog ORDER BY date_post DESC ''')
+    out = '<b>📩 Останні записи нашого блогу:</b>\n\n'
+    print(len(blog_table))
+    print(blog_table)
+    for blog_dict in blog_table[0:5]:
+        out += f"{blog_dict['title']} " \
+               f"<a href = \"{blog_dict['link']}\"> " + "➡️" + " </a>\n" \
+               f"<i>{str(blog_dict['date_post'])}</i>\n\n"
+    return out
