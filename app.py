f5d5362a0173480bed2297429b0b4002c5c8949c
ilkuzmenko <illia.kuzmenko@icloud.com>
2021-08-17 22:36:45 +0300

    Init blog_to_db() and add time check

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 import asyncio
+import time
 
 from loader import bot, create_user_db, create_blog_db
-from config import ADMIN_ID
-
-
-async def on_shutdown(dp):
-    await bot.close()
+from config import ADMIN
+from utils.web_scrapper.BlogScrapper import blog_to_db
 
 
 async def on_startup(dp):
+    start_time = time.time()
+    await blog_to_db(1)
+    print("--- %s seconds ---" % (time.time() - start_time))
     await asyncio.sleep(5)
     await create_user_db()
     await create_blog_db()
-    await bot.send_message(ADMIN_ID, "Bot on startup")
+    await bot.send_message(ADMIN, "Bot on startup")
 
 
 if __name__ == '__main__':
     from aiogram import executor
     from handlers import dp
 
-    executor.start_polling(dp, on_shutdown=on_shutdown, on_startup=on_startup)
+    executor.start_polling(dp, on_startup=on_startup)
