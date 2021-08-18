a48763dc1651f7815e926c4b5eeb87aa98bc5d9a
ilkuzmenko <illia.kuzmenko@icloud.com>
2021-08-18 20:15:24 +0300

    Update blog and commands with output blog

--- handlers/users/blog.py
+++ handlers/users/blog.py
@@ -1,22 +1,56 @@
 import logging
 
 from aiogram.dispatcher.filters import Text
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
+
+from keyboards.inline import blog
 from loader import dp, db
 from utils.db.comands import get_blog_page, subscribe_user
 
 logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
                     level=logging.INFO)
 
+user_data = {}
+
 
 @dp.message_handler(Text(equals=["📩 Отримати новини"]))
 async def btn_get_blog(message: Message):
-    blog_page = await get_blog_page()
-    await message.answer(blog_page, parse_mode='HTML')
+
+    user_data[message.from_user.id] = 1
+    blog_page = await get_blog_page(1)
+    await message.answer(blog_page, parse_mode='HTML', reply_markup=blog)
+    logging.info("btn_get_blog")
+
+
+async def update_num_text(message: Message, new_value: int):
+
+    blog_page = await get_blog_page(new_value)
+    await message.edit_text(blog_page, parse_mode='HTML', reply_markup=blog)
+    logging.info("update_num_text")
+
+
+@dp.callback_query_handler(Text(startswith="num_"))
+async def callbacks_num(call: CallbackQuery):
+
+    logging.info("pre callbacks")
+    first_page = user_data.get(call.from_user.id, 0)
+    action = call.data.split("_")[1]
+
+    if action == "incr":
+        user_data[call.from_user.id] = first_page+1
+        await update_num_text(call.message, first_page+1)
+    elif action == "decr":
+        user_data[call.from_user.id] = first_page-1
+        await update_num_text(call.message, first_page-1)
+    elif action == "finish":
+        await call.message.edit_text(first_page)
+    await call.answer()
+    logging.info("end callbacks")
 
 
 @dp.message_handler(Text(equals=["✅ Підписатися/❌ Відписатися"]))
 async def btn_subscribe(message: Message):
+
     if not bool(await db.pool.fetchval('SELECT blog FROM users WHERE user_id = $1', message.from_user.id)):
         await subscribe_user(True, message.from_user.id)
         await message.answer("✅ Ви успішно підписалися!")
