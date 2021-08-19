b8ae4ca95a2d441bccf41fd778c0803c085bae51
ilkuzmenko <illia.kuzmenko@icloud.com>
2021-08-19 13:28:48 +0300

    Add fill_blog_table

--- app.py
+++ app.py
@@ -4,19 +4,18 @@ import time
 from loader import bot
 from config import ADMIN
 from utils.db.Database import Database
-from utils.web_scrapper.BlogScrapper import blog_to_db
+from utils.web_scrapper.BlogScrapper import fill_blog_table
 
 
 async def on_startup(dp):
     await asyncio.sleep(5)
-
     db = Database()
     await db.connect()
     await db.create_table("user_table")
     await db.create_table("blog_table")
 
     start_time = time.time()
-    await blog_to_db(1)
+    await fill_blog_table()
     await bot.send_message(ADMIN, "Bot on startup")
     print("--- %s seconds ---" % (time.time() - start_time))
 
