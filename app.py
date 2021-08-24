9921f67a3e45b6b013d578727e0e382b80f60868
ilkuzmenko <illia.kuzmenko@icloud.com>
2021-08-24 23:17:30 +0300

    Add logging

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import asyncio
+import logging
 import time
 
 from loader import bot
 from config import ADMIN
 from utils.db.Database import Database
 from utils.web_scrapper.BlogScrapper import fill_blog_table
+
+logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
+                    level=logging.INFO)
 
 
 async def on_startup(dp):
@@ -17,7 +21,7 @@ async def on_startup(dp):
     start_time = time.time()
     await fill_blog_table()
     await bot.send_message(ADMIN, "Bot on startup")
-    print("--- %s seconds ---" % (time.time() - start_time))
+    logging.info("Bot on startup --- %s seconds ---" % (time.time() - start_time))
 
 
 if __name__ == '__main__':
