1c5bbf0aa61283d24eefd871efebde24ea8ef44b
ilkuzmenko <illia.kuzmenko@icloud.com>
2021-08-30 21:46:08 +0300

    Now we can add admins by split(', ')

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
-ADMIN = os.getenv("ADMIN")
+ADMINS = os.getenv("ADMINS")
 HOST = os.getenv("PGHOST")
 PG_USER = os.getenv("PG_USER")
 PG_PASS = os.getenv("PG_PASS")
