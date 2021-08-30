2b0fb62f2a4a348be65fbee4c7c9d93c5663d7f1
ilkuzmenko <illia.kuzmenko@icloud.com>
2021-08-30 21:43:17 +0300

    Add scheduler and notifyer

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import asyncio
 import logging
 import time
 
-from loader import bot
-from config import ADMIN
+from utils.scheduler import scheduler
 from utils.db.Database import Database
+from utils.notifyer import message_to_all, message_to_admins
 from utils.web_scrapper.BlogScrapper import fill_blog_table
 
 logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
@@ -12,15 +12,22 @@ logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(
 
 
 async def on_startup(dp):
-    await asyncio.sleep(5)
+    start_time = time.time()
+    asyncio.create_task(scheduler())
     db = Database()
+
+    await asyncio.sleep(0)
+
     await db.connect()
     await db.create_table("user_table")
     await db.create_table("blog_table")
 
-    start_time = time.time()
     await fill_blog_table()
-    await bot.send_message(ADMIN, "Bot on startup")
+
+    await message_to_all("Оновлення 1->20152.\nМи оновили функціонал, напишіть /start, щоб  оновлення вступили в силу.")
+
+    await message_to_admins("Bot on startup")
+
     logging.info("Bot on startup --- %s seconds ---" % (time.time() - start_time))
 
 
