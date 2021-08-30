69358d5334e170dfcd3d6dcd51ba73409d735d00
ilkuzmenko <illia.kuzmenko@icloud.com>
2021-08-30 21:50:21 +0300

    Add sheduler with scheduled messages.

---
+++ utils/scheduler.py
@@ -0,0 +1,22 @@
+import asyncio
+import logging
+import aioschedule
+
+from utils.db.comands import get_new_blog
+from utils.notifyer import message_to_subscribers
+
+logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
+                    level=logging.INFO)
+
+
+async def blog_message_schedule():
+    await message_to_subscribers(await get_new_blog())
+    logging.info("Blog message sent to users")
+
+
+async def scheduler():
+    logging.info("Init scheduler")
+    aioschedule.every().day.at("21:41").do(blog_message_schedule)
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(1)
