6d29a7c5530404606af45d56429ff771bef9155e
ilkuzmenko <illia.kuzmenko@icloud.com>
2021-09-03 12:37:15 +0300

    Add check for empty message string

--- utils/notifyer.py
+++ utils/notifyer.py
@@ -10,32 +10,35 @@ logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(
                     level=logging.INFO)
 
 
-async def message_to_all(message_text: str):
-    start_time = time.time()
-    users_table = await get_users()
+async def message_to_all(message_text):
+    if message_text:
+        start_time = time.time()
+        users_table = await get_users()
 
-    for user in users_table:
-        await bot.send_message(user['user_id'], message_text)
-
-    logging.info("Messages to all sent --- %s seconds ---" % (time.time() - start_time))
-
-
-async def message_to_admins(message_text: str):
-    start_time = time.time()
-    admins_list = ADMINS.split(", ")
-
-    for admin in range(len(admins_list)):
-        await bot.send_message(admins_list[admin], message_text)
-
-    logging.info("Messages to admins sent --- %s seconds ---" % (time.time() - start_time))
-
-
-async def message_to_subscribers(message_text: str):
-    start_time = time.time()
-    users_table = await get_users()
-
-    for user in users_table:
-        if user['blog']:
+        for user in users_table:
             await bot.send_message(user['user_id'], message_text)
 
-    logging.info("Messages to all sent --- %s seconds ---" % (time.time() - start_time))
+        logging.info("Messages to all sent --- %s seconds ---" % (time.time() - start_time))
+
+
+async def message_to_admins(message_text):
+    if message_text:
+        start_time = time.time()
+        admins_list = ADMINS.split(", ")
+
+        for admin in range(len(admins_list)):
+            await bot.send_message(admins_list[admin], message_text)
+
+        logging.info("Messages to admins sent --- %s seconds ---" % (time.time() - start_time))
+
+
+async def message_to_subscribers(message_text):
+    if message_text:
+        start_time = time.time()
+        users_table = await get_users()
+
+        for user in users_table:
+            if user['blog']:
+                await bot.send_message(user['user_id'], message_text)
+
+        logging.info("Messages to all sent --- %s seconds ---" % (time.time() - start_time))
