6fe3b2a3690a7db30ff981958e7dd3e1e55d572d
ilkuzmenko <illia.kuzmenko@icloud.com>
2021-10-13 10:02:19 +0300

    Del

--- keyboards/inline/blog.py
+++
@@ -1,5 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-blog = InlineKeyboardMarkup()
-blog.add(InlineKeyboardButton(text="⬅️", callback_data="num_decr"),
-         InlineKeyboardButton(text="➡️", callback_data="num_incr"))
