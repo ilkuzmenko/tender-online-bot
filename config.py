6e59f8bac9db7c904b08b1e7eb05c55df2e24cd9
ilkuzmenko <illia.kuzmenko@icloud.com>
2021-10-13 10:00:18 +0300

    Rename HOST to PG_HOST Add elastic const

--- config.py
+++ config.py
@@ -2,6 +2,9 @@ import os
 
 TOKEN = os.environ['TOKEN']
 ADMINS = os.environ['ADMINS']
-HOST = os.environ['PGHOST']
+PG_HOST = os.environ['PG_HOST']
 PG_USER = os.environ['PG_USER']
 PG_PASS = os.environ['PG_PASS']
+ES_HOST = os.environ['ES_HOST']
+ES_USER = os.environ['ES_USER']
+ES_PASS = os.environ['ES_PASS']
