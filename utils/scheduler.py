4743a0f0d4e01e182240833aac180fdc8768bb2b
ilkuzmenko <illia.kuzmenko@icloud.com>
2021-11-20 22:21:11 +0200

    Init scheduler: every day at 10:00 GMT

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 import aioschedule
 
-from utils.mydb.comands import get_news_page
+from utils.mydb.comands import get_new_blog
 from utils.notifyer import message_to_subscribers
 
 logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
@@ -11,13 +11,13 @@ logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(
 
 async def blog_message_schedule():
     """ Відпровляє повідомлення з новим дописом """
-    await message_to_subscribers(await get_news_page())
+    await message_to_subscribers(await get_new_blog())
     logging.info("Blog message sent to users")
 
 
 async def scheduler():
     """ Ініціалізує та запускає розклад перевірок нових дописів """
-    logging.info("Init scheduler")
+    logging.info("Init scheduler: every day at 10:00 GMT")
     aioschedule.every().day.at("10:00").do(blog_message_schedule)
     while True:
         await aioschedule.run_pending()
