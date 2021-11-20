9d469e83dffdff15b910b009539dcd1132436de1
ilkuzmenko <illia.kuzmenko@icloud.com>
2021-11-20 17:30:01 +0200

    Add check for launch app

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import asyncio
 import logging
+import os
 import time
 
 from utils.scheduler import scheduler
 from utils.mydb.Database import Database
-from utils.notifyer import message_to_all, message_to_admins
-from utils.web_scrapper.BlogScrapper import fill_blog_table
+from utils.notifyer import message_to_admins
+from utils.web_scrapper.NewsScrapper import fill_news_table
 
 logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
                     level=logging.INFO)
@@ -18,7 +19,7 @@ async def on_startup(dp):
     db = Database()
     await db.connect()
 
-    await fill_blog_table()
+    await fill_news_table()
 
     await message_to_admins("Bot on startup")
 
@@ -29,4 +30,16 @@ if __name__ == '__main__':
     from aiogram import executor
     from handlers import dp
 
-    executor.start_polling(dp, on_startup=on_startup)
+    path_log_file = 'logfile'
+
+    if not os.path.isfile(path_log_file):
+        with open(path_log_file, 'w') as file:
+            file.write('Bot launched!')
+        try:
+            executor.start_polling(dp, on_startup=on_startup)
+        finally:
+            os.remove(path_log_file)
+    else:
+        logging.info("Bot launched now. Finishing processes. Goodbye!")
+
+
