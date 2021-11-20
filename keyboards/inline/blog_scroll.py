da4a4cee9d749c0eaaf738858570deb97fa68778
ilkuzmenko <illia.kuzmenko@icloud.com>
2021-11-20 17:31:53 +0200

    refactoring

--- keyboards/inline/blog_scroll.py
+++
@@ -1,5 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-blog_scroll = InlineKeyboardMarkup()
-blog_scroll.add(InlineKeyboardButton(text="⬅️", callback_data="blog_decr"),
-                InlineKeyboardButton(text="➡️", callback_data="blog_incr"))
---
+++ keyboards/inline/news_scroll.py
@@ -0,0 +1,5 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+news_scroll = InlineKeyboardMarkup()
+news_scroll.add(InlineKeyboardButton(text="⬅️", callback_data="news_decr"),
+                InlineKeyboardButton(text="➡️", callback_data="news_incr"))
