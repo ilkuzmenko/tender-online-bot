9f3f213c44c7e6a150d8f834bf52f1168a3fb60b
ilkuzmenko <illia.kuzmenko@icloud.com>
2021-12-17 02:26:38 +0200

    Remove logging cfg

--- loader.py
+++ loader.py
@@ -1,5 +1,4 @@
 import asyncio
-import logging
 
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -7,8 +6,6 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from config import TOKEN
 from utils.mydb.Database import Database
 
-logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
-                    level=logging.INFO)
 
 loop = asyncio.get_event_loop()
 
@@ -19,4 +16,3 @@ dp = Dispatcher(bot, storage=storage)
 
 db = Database()
 loop.run_until_complete(db.connect())
-
